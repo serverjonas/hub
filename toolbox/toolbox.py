ab02f291574e1bf56a0a60fd3aa8544a17653dd5
naydrer89 <naydrer89@gmail.com>
2026-05-15 23:47:54 +0200

    update ui

--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -5,7 +5,7 @@ import time
 from flask import request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 
-BASE_PATH = "/var/www/serverjonas-hub"
+BASE_PATH = os.environ.get("SERVERJONAS_BASE", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BASE_DIR = BASE_PATH
 DB_PATH = os.path.join(BASE_PATH, "users.db")
 LOGS_DIR = os.path.join(BASE_DIR, "logs")
