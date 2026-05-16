d60e3d1d79a2cd522af3b6da3c4beb45aabe5631
serverjonas <dev@serverjonas.de>
2026-05-16 19:23:23 +0200

    fix interlan api/news

--- modules/api/internal/news/app.py
+++ modules/api/internal/news/app.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from toolbox.toolbox import get_notifications, 
+from toolbox.toolbox import get_notifications
 from toolbox.user import get_current_user, get_name
 
 bp = Blueprint("internal_api_news", __name__)
