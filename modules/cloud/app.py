9248b07d3eb2531d9ba2976f171d74e479dbd85f
serverjonas <dev@serverjonas.de>
2026-05-18 17:54:27 +0000

    fix sysntax odf modules cloud

--- modules/cloud/app.py
+++ modules/cloud/app.py
@@ -5,8 +5,8 @@ import zipfile
 import shutil
 from flask import Blueprint, render_template, request, abort, send_file, redirect, jsonify
 
-from toolbox.user import get_current_user, 
-from toolbox.toolbox import DATA_PATH
+from toolbox.user import get_current_user
+from toolbox.files import DATA_PATH
 
 bp = Blueprint("cloud", __name__, template_folder="templates")
 
--- toolbox/files.py
+++ toolbox/files.py
@@ -5,3 +5,4 @@ BASE_DIR = BASE_PATH
 DB_PATH = os.path.join(BASE_PATH, "users.db")
 LOGS_DIR = os.path.join(BASE_DIR, "logs")
 DATA_DIR = os.path.join(BASE_DIR, "data")
+DATA_PATH = DATA_DIR
