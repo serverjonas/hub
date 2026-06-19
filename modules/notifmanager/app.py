1b757fa5e84b9b3d025ae8f4a5ba616ee0cba4d4
serverjonas <jonas.hoeflinger@gmail.com>
2026-06-19 21:28:28 +0200

    fix notifmanager

--- modules/notifmanager/app.py
+++ modules/notifmanager/app.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import sys
 from flask import Blueprint, render_template, request, redirect, abort
-
+import sqlite3
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from toolbox.files import DB_PATH
